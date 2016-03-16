735dd45cacc46c05ab7218e98d709a8d1ddcfd37
AJ Henley <ajhenley@gmail.com>
2016-03-16 05:42:32 -0400

    AJ's Design updates added db seed data to show json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Testjson.create("title"=>"Morning Sales Meeting", "start"=>"Thu, 17 Mar 2016 09:00:00 UTC +00:00", "end"=>"Thu, 17 Mar 2016 10:00:00 UTC +00:00", "requestor"=>"Office Manager")
+Testjson.create("title"=>"Michelson's Contract Meeting", "start"=>"Thu, 17 Mar 2016 11:00:00 UTC +00:00", "end"=>"Thu, 17 Mar 2016 12:30:00 UTC +00:00", "requestor"=>"Managing Partner")
+Testjson.create("title"=>"Sara's Baby Shower", "start"=>"Thu, 17 Mar 2016 13:30:00 UTC +00:00", "end"=>"Thu, 17 Mar 2016 14:00:00 UTC +00:00", "requestor"=>"Hermione")
+Testjson.create("title"=>"Toastmaster's Meeting", "start"=>"Thu, 17 Mar 2016 17:00:00 UTC +00:00", "end"=>"Thu, 17 Mar 2016 18:30:00 UTC +00:00", "requestor"=>"Herb Gordon")
